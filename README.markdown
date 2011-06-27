a0d4f6a973e75e45c8cd39c4050416e146bfd635
Maurizio Lupo <mlupo@mlupo-laptop.(none)>
2011-06-27 17:40:18 +0200

    fix to plugin, added style, updated documentation

--- README.markdown
+++ README.markdown
@@ -1,45 +1,65 @@
 JQuery.spacetree
 ================
 
-This plugin transform a list of lists into a spacetree http://thejit.org
+This plugin transform a tree structured html into a spacetree using Javascript Infovis Toolkit (http://thejit.org)
+
+How to use
+==========
+Launch with:
+    $(htmltree_selector).spacetree(spacetree_selector, settings, jitsettings, onClickOptions);
+
+Minimal example:
+    $('#tree').spacetree('#spacetree');
 
 Parameters
 ==========
 
-Settings
---------
+htmltree_selector
+-----------------
+A css selector to the HTML tree.
 
-(see default_config for examples):
+spacetree_selector
+------------------
+A css selector pointing to the container where the spacetree will be drawn
+
+settings
+--------
+A Javascript object containing a group of function. These function will overrides the default functions used to extracting the tree informations.
+These are:
 
 - getData($node)
-returns an object with data ($content must be a jquery node)
+$node is a node of the html tree (jQuery object)
+returns an object. This object will be the 'data' object of each node (see the jit documentation).
+Important: the object must contain a '$content' field with the label to print on each spacetree node.
 
 - getId($node)
-returns the unique identifier of a node
+$node is a node of the html tree (jQuery object)
+returns the unique identifier of a node (string)
 
 - getChildrenNodes($node)
-returns a list of nodes
-
-- getTreeContainerId : function($node,index)
-It returns a container where I can place the spacetree
+$node is a node of the html tree (jQuery object)
+returns a list of nodes (sons of the current node)
 
 - getSelected: function($tree)
-It returns the selected node
-
+$tree is the first node of the html tree (jQuery object)
+It returns the selected node (a jQuery object)
 
 jitsettings
 -----------
 configuration of spacetree
 see the official documentation:
-
 - http://thejit.org/static/v20/Docs/files/Visualizations/Spacetree-js.html
 - http://thejit.org/static/v20/Docs/files/Options/Options-Controller-js.html#Options.Controller
 
+onClickOptions
+--------------
+See the onClick documentation on:
+http://thejit.org/static/v20/Docs/files/Visualizations/Spacetree-js.html
 
 Default html markup
 ===================
 
-This is the default html configuration:
+This is an example of the default html:
 
 
     <div id="tree">
--- jquery.spacetree.js
+++ jquery.spacetree.js
@@ -13,92 +13,17 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 // 02111-1307, USA.
 
-/*
-***********************************************
-*************** Show as tree ******************
-***********************************************
-
-This plugin transform a list of lists into a spacetree http://thejit.org
-
-**************************
-Parameters
-**************************
-
-settings
------------------------------------------------
-
-(see default_config for examples):
-
-- getData($node)
-returns an object with data ($content must be a jquery node)
-
-- getId($node)
-returns the unique identifier of a node
-
-- getChildrenNodes($node)
-returns a list of nodes
-
-- getTreeContainerId : function($node,index)
-It returns a container where I can place the spacetree
-
-- getSelected: function($tree)
-It returns the selected node
-
-
-jitsettings
--------------------
-configuration of spacetree
-see the official documentation
-http://thejit.org/static/v20/Docs/files/Visualizations/Spacetree-js.html
-http://thejit.org/static/v20/Docs/files/Options/Options-Controller-js.html#Options.Controller
-
-
-**************************
-Default html markup
-**************************
-
-This is the default html configuration:
-
-<div id="tree">
-    <div id="root">root element content</div>
-    <ul>
-        <li>
-            <div id="child1">child element content</div>
-            <ul>
-                <li>
-                    ...
-                </li>
-            </ul>
-        </li>
-        <li>
-            <div id="child2" class="selected">child element content</div>
-        </li>
-    </ul>
-</div>
-
-*/
-
-jQuery.fn.spacetree = function(settings, jitsettings, onClickOptions) {
+jQuery.fn.spacetree = function(selector, settings, jitsettings, onClickOptions) {
+    var st_placeholder = jQuery(selector).get(0);
     var default_config = {
         getData : function($node){
-            return {
-//                $content : $node.children('div').clone(true)
-                $content : $node.children('div')
-            }
+            return { $content : $node.children('div').clone(true)}
         },
         getId : function($node){
             return 'satree_' + $node.children('div').attr('id');
         },
         getChildrenNodes : function($node){
             return $node.children('ul').children();
-        },
-        getTreeContainerId : function($node,index){
-            var STId = 'spacetree' + index.toString();
-            jQuery('<div id="' + STId + '" />')
-                .width(600)
-                .height(400)
-                .insertAfter($node);
-            return STId
         },
         getSelected : function($tree){
             return $tree.find('.selected').parent();
@@ -133,58 +58,75 @@ jQuery.fn.spacetree = function(settings, jitsettings, onClickOptions) {
     var onClickConfig = jQuery.extend(default_onClickOptions,onClickOptions);
     var config = jQuery.extend(default_config,settings);
     
-    return this.each(function(index) {
-        // step 1 create json object for spacetree
-        var $firstnode = jQuery(this);
-        $firstnode.hide();
-        var nodeToJSON = function ($node){
-            var json = {
-                'id':config.getId($node),
-                'name':config.getId($node), // delete this ???
-                'data':config.getData($node),
-                'children':[]
-            };
-            config.getChildrenNodes($node).each(function (){
-                json.children.push(nodeToJSON(jQuery(this)));
-            });
-            
-            return json;
+
+    var $firstnode = this.eq(0);
+    // step 1 create json object for spacetree
+    
+    var selected = config.getSelected($firstnode);
+    var selected_id;
+    if(selected.length){
+        selected_id = config.getId(selected);
+    }
+
+    var nodeToJSON = function ($node){
+        var json = {
+            'id':config.getId($node),
+            'name':config.getId($node), 
+            'data':config.getData($node),
+            'children':[]
         };
+        config.getChildrenNodes($node).each(function (){
+            json.children.push(nodeToJSON(jQuery(this)));
+        });
+        
+        return json;
+    };
 
-        var json = nodeToJSON($firstnode);
-        //step 2 setting up spacetree
+    var json = nodeToJSON($firstnode);
+    //step 2 setting up spacetree
+    jitconfig.injectInto = st_placeholder;
+    jitconfig.onCreateLabel = function(label, node){  
+        label.id = node.id;              
+        node.data.$content
+        .appendTo(label)
+        .width(node.Config.width)
+        .height(node.Config.height);
 
-        jitconfig.injectInto = config.getTreeContainerId($firstnode,index)
-        jitconfig.onCreateLabel = function(label, node){  
-            label.id = node.id;              
-            node.data.$content
-            .appendTo(label)
-            .width(node.Config.width)
-            .height(node.Config.height);
-
-            label.onclick = function(){  
-                st.onClick(node.id,onClickConfig);  
-            };  
+        if(label.id === selected_id){
+            jQuery(label).addClass('selectednode');
         }
 
-        var st = new $jit.ST(jitconfig);
+        label.onclick = function(){
+            jQuery(st_placeholder).find('.node').removeClass('selectednode');
+            jQuery(this).addClass('selectednode');
 
-        //load json data  
-        st.loadJSON(json);  
-        //compute node positions and layout  
-        st.compute();
+            st.onClick(node.id,onClickConfig);  
+        };  
+    }
 
-        var selected = config.getSelected($firstnode);
+    var st = new $jit.ST(jitconfig);
 
-        if(selected.length){
-            st.onClick(config.getId(selected),onClickConfig); 
-        }
-        else{
-            //emulate a click on the root node.  
-            st.onClick(st.root,onClickConfig); 
-        }
+    //load json data  
+    st.loadJSON(json);  
+    //compute node positions and layout  
+    st.compute();
 
+    var selected = config.getSelected($firstnode);
 
+    if(selected_id){
+        st.onClick(selected_id,onClickConfig); 
+    }
+    else{
+        //emulate a click on the root node.  
+        st.onClick(st.root,onClickConfig); 
+    }
+
+    jQuery(window).resize(function (){
+        var $win = jQuery(st_placeholder);
+        var w = $win.width(), h = $win.height();
+        st.canvas.resize(w, h); 
     });
+    return this;
+
 };
 
